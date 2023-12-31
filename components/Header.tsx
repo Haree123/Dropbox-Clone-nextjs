c2c30551bbf573d1a2ff14a51540ee0c1b74f4c9
Haree <haree@addup.network>
2023-12-31 12:36:20 +0530

    initial commit and added Header Component

---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { ThemeToggler } from "./Theme-Toggler";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="flex justify-between items-center">
+        <div className="flex items-center space-x-1">
+          <div className="bg-[#0160FE]">
+            <Image
+              className="invert"
+              src="https://www.shareicon.net/download/2016/07/13/606936_dropbox_2048x2048.png"
+              alt="Img"
+              height={50}
+              width={50}
+              loading="lazy"
+            />
+          </div>
+          <h3 className="font-bold text-xl">Dropbox</h3>
+        </div>
+
+        <div className="flex px-2 items-center space-x-4">
+          <ThemeToggler />
+
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton mode="modal" afterSignInUrl="/dashboard" />
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
